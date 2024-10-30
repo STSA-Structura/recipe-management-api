27d3a1eff9bbbee73c7a1c6994884415b0b5dcfe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-30 22:54:59 +0530

    Code Refactoring

--- tests/RecipeManagement.Api.Tests/Test1.cs
+++ tests/RecipeManagement.Api.Tests/Test1.cs
@@ -1,47 +1,46 @@
-﻿namespace RecipeManagement.Api.Tests
+﻿namespace RecipeManagement.Api.Tests;
+
+[TestClass]
+public sealed class Test1
 {
-    [TestClass]
-    public sealed class Test1
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
     {
-        [AssemblyInitialize]
-        public static void AssemblyInit(TestContext context)
-        {
-            // This method is called once for the test assembly, before any tests are run.
-        }
+        // This method is called once for the test assembly, before any tests are run.
+    }
 
-        [AssemblyCleanup]
-        public static void AssemblyCleanup()
-        {
-            // This method is called once for the test assembly, after all tests are run.
-        }
+    [AssemblyCleanup]
+    public static void AssemblyCleanup()
+    {
+        // This method is called once for the test assembly, after all tests are run.
+    }
 
-        [ClassInitialize]
-        public static void ClassInit(TestContext context)
-        {
-            // This method is called once for the test class, before any tests of the class are run.
-        }
+    [ClassInitialize]
+    public static void ClassInit(TestContext context)
+    {
+        // This method is called once for the test class, before any tests of the class are run.
+    }
 
-        [ClassCleanup]
-        public static void ClassCleanup()
-        {
-            // This method is called once for the test class, after all tests of the class are run.
-        }
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        // This method is called once for the test class, after all tests of the class are run.
+    }
 
-        [TestInitialize]
-        public void TestInit()
-        {
-            // This method is called before each test method.
-        }
+    [TestInitialize]
+    public void TestInit()
+    {
+        // This method is called before each test method.
+    }
 
-        [TestCleanup]
-        public void TestCleanup()
-        {
-            // This method is called after each test method.
-        }
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        // This method is called after each test method.
+    }
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+    [TestMethod]
+    public void TestMethod1()
+    {
     }
 }
