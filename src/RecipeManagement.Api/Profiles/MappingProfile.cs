82832658639f115f5c98ffd91b53ee5157b539e4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-31 13:37:01 +0530

    Auto Mapper

--- src/RecipeManagement.Api/Profiles/MappingProfile.cs
+++ src/RecipeManagement.Api/Profiles/MappingProfile.cs
@@ -1,5 +1,15 @@
-﻿namespace RecipeManagement.Api.Profiles;
+﻿using AutoMapper;
+using RecipeManagement.Api.Dtos;
+using RecipeManagement.Api.Models;
 
-public class MappingProfile
+namespace RecipeManagement.Api.Profiles;
+
+public class MappingProfile : Profile
 {
+    public MappingProfile()
+    {
+        CreateMap<Recipe, RecipeDto>();
+        CreateMap<RecipeCreateDto, Recipe>();
+        CreateMap<RecipeUpdateDto, Recipe>();
+    }
 }
--- src/RecipeManagement.Api/Program.cs
+++ src/RecipeManagement.Api/Program.cs
@@ -1,11 +1,16 @@
+using RecipeManagement.Api.Profiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register AutoMapper with MappingProfile
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
