cfaaa0ac64b2376df4a840063c2a7b46119d3822
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-31 12:28:14 +0530

    Create RecipeCreateDto.cs

---
+++ src/RecipeManagement.Api/Dtos/RecipeCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeManagement.Api.Dtos;
+
+public class RecipeCreateDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public List<string> Ingredients { get; set; } = [];
+
+    public int DifficultyLevel { get; set; }
+}
