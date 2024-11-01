ca1ef9dd2097298838e17c58841b8f1bb465ffa9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-01 22:55:10 +0530

    Implemented Repository

---
+++ src/RecipeManagement.Api/Repositories/Implementations/RecipeRepository.cs
@@ -0,0 +1,53 @@
+﻿using RecipeManagement.Api.Entities.Recipes;
+using RecipeManagement.Api.Repositories.Interfaces;
+
+namespace RecipeManagement.Api.Repositories.Implementations;
+
+public class RecipeRepository : IRecipeRepository
+{
+    private readonly List<Recipe> _recipes = new();
+
+    public Task<List<Recipe>> GetAllRecipesAsync()
+    {
+        return Task.FromResult(_recipes);
+    }
+
+    public Task<Recipe?> GetRecipeByIdAsync(Guid id)
+    {
+        var recipe = _recipes.FirstOrDefault(r => r.Id == id);
+        return Task.FromResult(recipe);
+    }
+
+    public Task AddRecipeAsync(Recipe recipe)
+    {
+        recipe.Id = Guid.NewGuid();
+        _recipes.Add(recipe);
+
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateRecipeAsync(Recipe updatedRecipe)
+    {
+        var recipe = _recipes.FirstOrDefault(r => r.Id == updatedRecipe.Id);
+        if (recipe != null)
+        {
+            recipe.Name = updatedRecipe.Name;
+            recipe.Description = updatedRecipe.Description;
+            recipe.Ingredients = updatedRecipe.Ingredients;
+            recipe.DifficultyLevel = updatedRecipe.DifficultyLevel;
+            recipe.UpdatedAt = DateTime.UtcNow;
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteRecipeAsync(Guid id)
+    {
+        var recipe = _recipes.FirstOrDefault(r => r.Id == id);
+        if (recipe != null)
+        {
+            _recipes.Remove(recipe);
+        }
+        return Task.CompletedTask;
+    }
+
+}
---
+++ src/RecipeManagement.Api/Repositories/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,16 @@
+﻿using RecipeManagement.Api.Entities.Recipes;
+
+namespace RecipeManagement.Api.Repositories.Interfaces;
+
+public interface IRecipeRepository
+{
+    Task<List<Recipe>> GetAllRecipesAsync();
+
+    Task<Recipe?> GetRecipeByIdAsync(Guid id);
+
+    Task AddRecipeAsync(Recipe recipe);
+
+    Task UpdateRecipeAsync(Recipe recipe);
+
+    Task DeleteRecipeAsync(Guid id);
+}
